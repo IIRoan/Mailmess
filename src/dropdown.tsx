3034a866508c3155ae8cbfc8c373c1dd53fb3563
IIRoan <github.com.q6c0y@lunary.roan.zip>
2024-11-12 14:49:16 +0100

    Added options length of random string, added favicon to show what website you are on

--- src/dropdown.tsx
+++ src/dropdown.tsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   InputField,
-  Toggle,
   IconButton,
   Icon,
   Size,
+  Dropdown,
+  DropdownItem,
+  Button,
+  Type,
+  Avatar,
+  IconText,
+  Divider,
+  ThemeMode,
 } from "@skiff-org/skiff-ui";
 import "./styles.css";
 
 function EmailGenerator() {
   // State to track the current tab's domain
   const [domain, setDomain] = useState("");
-  // State to track if the random string toggle is on
-  const [isRandomStringEnabled, setIsRandomStringEnabled] = useState(() => {
-    const savedToggleState = localStorage.getItem("isRandomStringEnabled");
-    return savedToggleState ? JSON.parse(savedToggleState) : false;
-  });
   // State to hold the email suffix entered by the user
   const [emailSuffix, setEmailSuffix] = useState("");
   // State to hold the complete generated email
@@ -27,6 +29,43 @@ function EmailGenerator() {
   const [icon, setIcon] = useState(Icon.Copy);
   // State to hold the error message underneath input
   const [errorMessage, setErrorMessage] = useState("");
+  // State to hold the random string length
+  const [randomStringLength, setRandomStringLength] = useState(() => {
+    const savedLength = localStorage.getItem("randomStringLength");
+    return savedLength ? Number(savedLength) : 0;
+  });
+
+  // State to hold the favicon URL
+  const [faviconUrl, setFaviconUrl] = useState('');
+
+  // Reference for the dropdown button
+  const buttonRef = useRef(null);
+  // State to control the visibility of the dropdown
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  // Options for the random string length
+  const randomStringOptions = [
+    {
+      value: 0,
+      label: "No random characters",
+      icon: Icon.RadioEmpty,
+    },
+    {
+      value: 3,
+      label: "Add 3 random characters",
+      icon: Icon.Lock,
+    },
+    {
+      value: 5,
+      label: "Add 5 random characters",
+      icon: Icon.Lock,
+    },
+    {
+      value: 7,
+      label: "Add 7 random characters",
+      icon: Icon.Lock,
+    },
+  ];
 
   // Helper function to generate a random alphanumeric string
   const generateRandomString = (length: number) => {
@@ -35,15 +74,18 @@ function EmailGenerator() {
     let text = "";
     for (let i = 0; i < length; i++) {
       text += possibleChars.charAt(
-        Math.floor(Math.random() * possibleChars.length),
+        Math.floor(Math.random() * possibleChars.length)
       );
     }
     return text;
   };
 
+
+
+
   // Handler for changes in the email suffix input field
   const handleEmailSuffixChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setEmailSuffix(event.target.value);
     setErrorMessage(""); // Clear error message on input change
@@ -55,13 +97,10 @@ function EmailGenerator() {
     return emailRegex.test(email);
   };
 
-  // Effect hook to update local storage when the toggle state changes
+  // Effect hook to update local storage when the randomStringLength changes
   useEffect(() => {
-    localStorage.setItem(
-      "isRandomStringEnabled",
-      JSON.stringify(isRandomStringEnabled),
-    );
-  }, [isRandomStringEnabled]);
+    localStorage.setItem("randomStringLength", randomStringLength.toString());
+  }, [randomStringLength]);
 
   // Effect hook to change icon on copy generated email
   useEffect(() => {
@@ -75,31 +114,24 @@ function EmailGenerator() {
     }
   }, [animationTrigger]);
 
-  // Effect hook to load previously stored email suffix and generate the email
-  useEffect(() => {
-    const storedSuffix = localStorage.getItem("emailSuffix");
-    if (storedSuffix) {
-      setEmailSuffix(storedSuffix);
-      const randomSuffix = isRandomStringEnabled
-        ? `-${generateRandomString(5)}`
-        : "";
-      setGeneratedEmail(`${domain}${randomSuffix}${storedSuffix}`);
-    }
-  }, [isRandomStringEnabled, domain]);
 
-  // Function to get the hostname of the currently active tab
+  // Function to get the hostname and favicon of the currently active tab
   const getActiveTabHostname = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0 && tabs[0].url) {
         const parsedUrl = new URL(tabs[0].url);
         let hostname = parsedUrl.hostname;
 
-        // Attempt to extract the main part of the domain
+        // Extract the main part of the domain
         const domainParts = hostname.split(".");
-        let mainPart = domainParts.slice(-2, -1)[0]; // This attempts to get the second last part of the domain
+        let mainPart = domainParts.slice(-2, -1)[0];
 
-        // This simple approach has limitations and may not work correctly for all domain structures
         setDomain(mainPart);
+
+        // Get the favicon URL
+        if (tabs[0].favIconUrl) {
+          setFaviconUrl(tabs[0].favIconUrl);
+        }
       }
     });
   };
@@ -117,12 +149,17 @@ function EmailGenerator() {
       domainAndSubdomain = `@${matches[1]}.${matches[2]}`;
     }
 
-    const fullEmail = `${domain}${domainAndSubdomain}`;
+    const randomSuffix =
+      randomStringLength > 0
+        ? `-${generateRandomString(randomStringLength)}`
+        : "";
+
+    const fullEmail = `${domain}${randomSuffix}${domainAndSubdomain}`;
     if (validateEmail(fullEmail)) {
       localStorage.setItem("emailSuffix", domainAndSubdomain);
       setGeneratedEmail(fullEmail);
     } else {
-      setErrorMessage("Invalid E-Mail suffix");
+      setErrorMessage("Invalid email suffix");
     }
   };
 
@@ -137,15 +174,56 @@ function EmailGenerator() {
     }
   };
 
+  // Effect hook to update generated email whenever dependencies change
+  useEffect(() => {
+    const storedSuffix = localStorage.getItem("emailSuffix");
+    let suffix = emailSuffix;
+    if (storedSuffix) {
+      suffix = storedSuffix;
+      setEmailSuffix(storedSuffix);
+    }
+    const randomSuffix =
+      randomStringLength > 0
+        ? `-${generateRandomString(randomStringLength)}`
+        : "";
+    const domainPartRegex = /@([^.]+)\.(.*)/;
+    const matches = suffix.match(domainPartRegex);
+    let domainAndSubdomain = "";
+    if (matches && matches.length >= 3) {
+      domainAndSubdomain = `@${matches[1]}.${matches[2]}`;
+    }
+
+    const fullEmail = `${domain}${randomSuffix}${domainAndSubdomain}`;
+    setGeneratedEmail(fullEmail);
+  }, [domain, randomStringLength, emailSuffix]);
+
+  // Get the label for the current random string length
+  const currentOption = randomStringOptions.find(
+    (option) => option.value === randomStringLength
+  );
+
+
   return (
     <>
       <header className="mainbody">
-        <img src="static/logo192.png" alt="MailMess Logo"></img>
-
+        <h1 className="title">MailMess</h1>
+        {faviconUrl && (
+          <img
+            src={faviconUrl}
+            alt="favicon"
+            style={{
+              position: 'absolute',
+              top: '8px',
+              right: '8px',
+              width: '16px',
+              height: '16px',
+            }}
+          />
+        )}
         <div className="input-container">
           <InputField
             icon={Icon.Envelope}
-            placeholder="Enter email suffix"
+            placeholder="Enter email suffix (e.g., @example.com)"
             value={emailSuffix}
             onChange={handleEmailSuffixChange}
           />
@@ -172,16 +250,50 @@ function EmailGenerator() {
           />
         </div>
 
-        <div className="toggle">
-          <Toggle
-            checked={isRandomStringEnabled}
-            onChange={() => setIsRandomStringEnabled(!isRandomStringEnabled)}
-          />
-          <span>Add 5 random letters/numbers after domain</span>
+        <div className="dropdown-container">
+          <Button
+            ref={buttonRef}
+            type={Type.SECONDARY}
+            onClick={() => setShowDropdown(!showDropdown)}
+            fullWidth
+          >
+            {currentOption
+              ? currentOption.label
+              : "Select random character option"}
+          </Button>
+          <Dropdown
+            portal
+            buttonRef={buttonRef}
+            setShowDropdown={setShowDropdown}
+            showDropdown={showDropdown}
+            width={350}
+            gapFromAnchor={4}
+          >
+            {randomStringOptions.map((option, index) => (
+              <React.Fragment key={option.value}>
+                <DropdownItem
+                  label={
+                    <IconText
+                      startIcon={option.icon}
+                      label={option.label}
+                      color='white'
+                    />
+                  }
+                  onClick={() => {
+                    setRandomStringLength(option.value);
+                    setShowDropdown(false);
+                  }}
+                  active={randomStringLength === option.value}
+                />
+                {index < randomStringOptions.length - 1 && (
+                  <Divider forceTheme={ThemeMode.DARK} />
+                )}
+              </React.Fragment>
+            ))}
+          </Dropdown>
         </div>
       </header>
     </>
   );
 }
-
 export default EmailGenerator;
